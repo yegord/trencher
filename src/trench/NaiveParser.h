bd79f49e7fb01d38fc1a95245fb3f8243adf86b3
ederevenetc <ederevenetc@52385686-05bb-4bd5-9536-738c8841fd5f>
2012-07-16 13:41:58 +0000

    License note added
    git-svn-id: https://rad.cs.uni-kl.de/svn/ederevenetc/trencher@333 52385686-05bb-4bd5-9536-738c8841fd5f

--- src/trench/NaiveParser.h
+++ src/trench/NaiveParser.h
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE JUICE-WARE LICENSE" (Revision 42):
+ * <derevenetc@cs.uni-kl.de> wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a glass of juice in return.
+ * ----------------------------------------------------------------------------
+ */
+
 #pragma once
 
 #include "Parser.h"
